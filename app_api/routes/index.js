ef6fd791e4e0f5301b2dc81f107838bbbed1dbf4
Cathy Hawley <xcathii@gmail.com>
2025-02-14 16:12:53 -0500

    Module 6 completed baseline

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -4,12 +4,14 @@ const router = express.Router();
 const tripsController = require("../controllers/trips");
 
 router
-    .route("/trips")
-    .get(tripsController.tripsList); // GET Method routes tripList
+    .route('/trips')
+    .get(tripsController.tripsList) // GET Method routes tripList
+    .post(tripsController.tripsAddTrip); // POST Method adds a trip
 
 // GET Method routes tripsFindByCode - requires parameter
 router
     .route('/trips/:tripCode')
-    .get(tripsController.tripsFindByCode);
+    .get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip);
         
 module.exports = router;
